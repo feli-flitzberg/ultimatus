18e3326fd8be63085acc76c6b69660e0ace0f022
Feli Flitzberg <feliflitzberg@proton.me>
2025-01-21 23:28:50 -0700

    adjusted export settings; verified browser issue with firefox; tweaks and fixes

--- deployment/index.service.worker.js
+++ deployment/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737497485|14788803996';
+const CACHE_VERSION = '1737518192|23395301185';
 /** @type {string} */
 const CACHE_PREFIX = 'ULTIMATUS-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
