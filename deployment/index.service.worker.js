cc22697a964f9447324c4f3b2327b5db6c876fdd
Feli Flitzberg <feliflitzberg@proton.me>
2025-01-22 23:14:20 -0700

    MINIMAL VIABLE GAME: base mechanics are present, game can now be extended as time allows

--- deployment/index.service.worker.js
+++ deployment/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737518192|23395301185';
+const CACHE_VERSION = '1737612658|66584286565';
 /** @type {string} */
 const CACHE_PREFIX = 'ULTIMATUS-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
---
+++ scripts/signals.gd
@@ -0,0 +1,37 @@
+extends Node
+
+# player/enemy signals
+signal pos(playerpos: Vector2)
+signal wipe(wattack, wslow)
+signal heal(value)
+signal addscore(value)
+signal dead
+
+# ui signals
+signal startrun
+signal started
+signal endrun(score, time)
+
+# reusing the autoload for stats/settings functions
+var config = ConfigFile.new()
+var conf = configure()
+func configure():
+	var new = config
+	var err = new.load("user://settings.ini")
+	if err != OK:
+		new.set_value("Scores", "hiscore", 0)
+		new.set_value("Scores", "hitime", 0)
+		new.set_value("Scores", "curscore", 0)
+		new.set_value("Scores", "curtime", 0)
+		new.save("user://settings.ini")
+	return new
+func save(score, time):
+	var scorecheck = conf.get_value("Scores", "hiscore", 0)
+	if score > scorecheck:
+		conf.set_value("Scores", "hiscore", score)
+	var timecheck = conf.get_value("Scores", "hitime", 0)
+	if time > timecheck:
+		conf.set_value("Scores", "hitime", time)
+	conf.set_value("Scores", "curscore", score)
+	conf.set_value("Scores", "curtime", time)
+	conf.save("user://settings.ini")
