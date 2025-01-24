ddd866429451798d1e6bcb34537751f16628e474
Feli Flitzberg <feliflitzberg@proton.me>
2025-01-23 20:40:16 -0700

    fixed character movement on death; added basic screen flash to indicate attack; added remaining mobs and boss

--- deployment/index.service.worker.js
+++ deployment/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737614029|67955670580';
+const CACHE_VERSION = '1737689333|9668654164';
 /** @type {string} */
 const CACHE_PREFIX = 'ULTIMATUS-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
--- scripts/signals.gd
+++ scripts/signals.gd
@@ -20,18 +20,22 @@ func configure():
 	var err = new.load("user://settings.ini")
 	if err != OK:
 		new.set_value("Scores", "hiscore", 0)
+		new.set_value("Scores", "hiscoretime", 0)
 		new.set_value("Scores", "hitime", 0)
+		new.set_value("Scores", "hitimescore", 0)
 		new.set_value("Scores", "curscore", 0)
 		new.set_value("Scores", "curtime", 0)
 		new.save("user://settings.ini")
 	return new
-func save(score, time):
+func scoresave(score, time):
 	var scorecheck = conf.get_value("Scores", "hiscore", 0)
 	if score > scorecheck:
 		conf.set_value("Scores", "hiscore", score)
+		conf.set_value("Scores", "hiscoretime", time)
 	var timecheck = conf.get_value("Scores", "hitime", 0)
 	if time > timecheck:
 		conf.set_value("Scores", "hitime", time)
+		conf.set_value("Scores", "hitimescore", score)
 	conf.set_value("Scores", "curscore", score)
 	conf.set_value("Scores", "curtime", time)
 	conf.save("user://settings.ini")
